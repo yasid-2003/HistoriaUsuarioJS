99d6700f97620c8854d0aebd0ef863d84608cafa
angel jimenez <angelyasid2003@gmail.com>
2026-05-29 17:51:32 -0500

    crear proyecto

--- client/src/main.js
+++ client/src/main.js
@@ -1,1 +1,9 @@
 import "./styles/global.css";
+
+const formulario = document.getElementById("registro");
+const registroNombre = document.getElementById("register-name");
+const regisApellido = document.getElementById("register-lastname")
+const regisCorreo = document.getElementById("register-email");
+const password = document.getElementById("register-password");
+const rol = document.getElementById("register-role");
+
